447c8deb2903eef64801792007db9378eab70b58
aleksandrina-k <61983520+aleksandrina-k@users.noreply.github.com>
2022-03-23 11:40:28 +0200

    Add files via upload

--- JsonableAndXmlable/json_xml_demo.py
+++ JsonableAndXmlable/json_xml_demo.py
@@ -2,42 +2,35 @@ import json
 from typing import Iterable
 
 
+class NotValidObjectType(Exception):
+    print("NotValidObjectType")
+
+
 class Jsonable:
+    # from python to json
     @staticmethod
     def from_iter_to_dictionary(itr):
         result_list = []
         for el in itr:
             if isinstance(el, Jsonable):
                 result_list.append(el.to_dictionary())
+            elif isinstance(el, dict):
+                result_list.append(Jsonable.dict_to_json_dict(el))
+            elif isinstance(el, Iterable) and not isinstance(el, str):
+                result_list.append(Jsonable.from_iter_to_dictionary(el))
             else:
                 result_list.append(el)
         return result_list
 
     @staticmethod
-    def dict_to_json(d: dict):
+    def dict_to_json_dict(d: dict):
         result_dict = {}
 
         for key, value in d.items():
             if isinstance(value, Jsonable):
                 result_dict[key] = value.to_dictionary()
             elif isinstance(value, dict):
-                result_dict[key] = Jsonable.check_dict(value)
-            elif isinstance(value, Iterable) and not isinstance(value, str):
-                result_dict[key] = Jsonable.from_iter_to_dictionary(value)
-            else:
-                result_dict[key] = value
-
-        return result_dict
-
-    @staticmethod
-    def dict_to_obj(d: dict):
-        result_dict = {}
-
-        for key, value in d.items():
-            if isinstance(value, Jsonable):
-                result_dict[key] = value.to_dictionary()
-            elif isinstance(value, dict):
-                result_dict[key] = Jsonable.check_dict(value)
+                result_dict[key] = Jsonable.dict_to_json_dict(value)
             elif isinstance(value, Iterable) and not isinstance(value, str):
                 result_dict[key] = Jsonable.from_iter_to_dictionary(value)
             else:
@@ -57,7 +50,7 @@ class Jsonable:
             if isinstance(value, Jsonable):
                 dictionary["dict"][attrib] = value.to_dictionary()
             elif isinstance(value, dict):
-                dictionary["dict"][attrib] = Jsonable.dict_to_json(value)
+                dictionary["dict"][attrib] = Jsonable.dict_to_json_dict(value)
             elif isinstance(value, Iterable) and not isinstance(value, str):
                 dictionary["dict"][attrib] = Jsonable.from_iter_to_dictionary(value)
             else:
@@ -65,10 +58,52 @@ class Jsonable:
 
         return dictionary
 
+    # from json to python
+    @staticmethod
+    def get_iter_values(itr: Iterable):
+        values_list = []
+        for el in itr:
+            if Jsonable.is_object(el):
+                values_list.append(Jsonable.to_object(el))
+            elif isinstance(el, dict):
+                values_list.append(Jsonable.get_dict_values(el))
+            elif isinstance(el, Iterable) and not isinstance(el, str):
+                values_list.append(Jsonable.get_iter_values(el))
+            else:
+                values_list.append(el)
+
+        return values_list
+
+    @staticmethod
+    def get_dict_values(d: dict):
+        values_dict = {}
+
+        for key, value in d.items():
+            if Jsonable.is_object(value):
+                values_dict[key] = Jsonable.to_object(value)
+            elif isinstance(value, dict):
+                values_dict[key] = Jsonable.get_dict_values(value)
+            elif isinstance(value, Iterable) and not isinstance(value, str):
+                values_dict[key] = Jsonable.get_iter_values(value)
+            else:
+                values_dict[key] = value
+
+        return values_dict
+
+    @staticmethod
+    def is_object(variable):
+        if isinstance(variable, dict):
+            if "dict" in variable.keys():
+                return True
+        return False
+
     @classmethod
     def from_json(cls, json_string: str):
         python_dict = json.loads(json_string)
-        print(python_dict)
+
+        if cls.__name__ != python_dict["type"]:
+            raise NotValidObjectType
+
         object = Jsonable.to_object(python_dict)
 
         return object
@@ -82,66 +117,48 @@ class Jsonable:
             if Jsonable.is_object(value):
                 param_values_list.append(Jsonable.to_object(value))
             elif isinstance(value, dict):
-                var_dict = {}
-                for k, v in value.items():
-
-
+                param_values_list.append(Jsonable.get_dict_values(value))
             elif isinstance(value, Iterable) and not isinstance(value, str):
-                var_list = []
-                for el in value:
-                    if Jsonable.is_object(el):
-                        var_list.append(Jsonable.to_object(el))
-
-                    else:
-                        var_list.append(el)
-                param_values_list.append(var_list)
-
+                param_values_list.append(Jsonable.get_iter_values(value))
             else:
                 param_values_list.append(value)
 
         return eval(obj_class)(*param_values_list)
 
-    @staticmethod
-    def is_object(variable):
-        if isinstance(variable, dict):
-            if "dict" in variable.keys():
-                return True
-        return False
+
+# class Xmlable:
+#     def to_xml(self):
+#         xml_list = [f'<{self.__class__.__name__}>']
+#         for param, value in self.__dict__.items():
+#             element = '<' + param + '>' + str(value) + '</' + param + '>'
+#             xml_list.append(element)
+#
+#         xml_list.append(f'</{self.__class__.__name__}>')
+#
+#         return ''.join(xml_list)
+#
+#     @classmethod
+#     def from_xml(cls, xml_string):
+#         xml_string = xml_string.replace('<', ' ')
+#         xml_string = xml_string.replace('>', ' ')
+#         print(xml_string)
+#         xml_list = xml_string.split('  ')
+#
+#         arg_values = []
+#         for attrib in xml_list[1:-1]:
+#             arg_values.append(attrib.split(' ')[1])
+#
+#         # print(arg_values)
+#         try:
+#             new_object = eval(cls.__name__)(*arg_values)
+#             return new_object
+#         except TypeError:
+#             raise ValueError
+#         except Exception:
+#             print('Error')
 
 
-class Xmlable:
-    def to_xml(self):
-        xml_list = [f'<{self.__class__.__name__}>']
-        for param, value in self.__dict__.items():
-            element = '<' + param + '>' + str(value) + '</' + param + '>'
-            xml_list.append(element)
-
-        xml_list.append(f'</{self.__class__.__name__}>')
-
-        return ''.join(xml_list)
-
-    @classmethod
-    def from_xml(cls, xml_string):
-        xml_string = xml_string.replace('<', ' ')
-        xml_string = xml_string.replace('>', ' ')
-        print(xml_string)
-        xml_list = xml_string.split('  ')
-
-        arg_values = []
-        for attrib in xml_list[1:-1]:
-            arg_values.append(attrib.split(' ')[1])
-
-        # print(arg_values)
-        try:
-            new_object = eval(cls.__name__)(*arg_values)
-            return new_object
-        except TypeError:
-            raise ValueError
-        except Exception:
-            print('Error')
-
-
-class Panda(Jsonable, Xmlable):
+class Panda(Jsonable):
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -150,18 +167,16 @@ class Panda(Jsonable, Xmlable):
         return self.name == other.name and self.age == other.age
 
 
-class Person(Jsonable, Xmlable):
-    def __init__(self, first_name: str, last_name: str, age: int, friends: list, panda: Panda):
-        self.first_name = first_name
-        self.last_name = last_name
+class Person(Jsonable):
+    def __init__(self, name: str, age: int, friends: list, children: dict, panda: Panda):
+        self.name = name
         self.age = age
         self.friends = friends
-        self.children = {'boys': {"count": 2, "children": ['plamen', 'pavel']}, 'girls': {"count": 0, "children": []}}
+        self.children = children
         self.panda = panda
 
     def __eq__(self, other):
-        return self.first_name == other.first_name and \
-               self.last_name == other.last_name and \
+        return self.name == other.name and \
                self.age == other.age and \
                self.friends == other.friends and \
                self.children == other.children and \
@@ -170,11 +185,13 @@ class Person(Jsonable, Xmlable):
 
 if __name__ == '__main__':
     ivcho = Panda('ivcho', 2)
-    goshko = Person('goshko', 'goshev', 5, [], ivcho)
-    joro = Person('joro', 'georgiev', 20, ['sasho', 'stoyan', 'svetlio', goshko], ivcho)
-    joro.children["boys"]["children"].append(goshko)
+    ivan = Person('goshko', 5, [], {}, ivcho)
+    joro = Person('joro', 20,
+                  ['stoyan', 'svetlio', ivan],
+                  {'boys': {"count": 3, "children": ['plamen', 'pavel', ivan]}, 'girls': {"count": 0, "children": []}}
+                  , ivcho)
 
-    print(joro.to_json())
-    new_joro = Jsonable.from_json(joro.to_json())
+    # print(joro.to_json())
 
-    # print(joro == new_joro)
+    new_joro = Person.from_json(ivcho.to_json())
+    print(new_joro.__dict__)
--- JsonableAndXmlable/json_xml_demo_test.py
+++ JsonableAndXmlable/json_xml_demo_test.py
@@ -6,7 +6,7 @@ class TestJsonXml(unittest.TestCase):
 
     def setUp(self):
         self.panda = Panda('name', 1)
-        self.person = Person('firstname', 'lastname', 15, ['aaa', 'bbb', self.panda], self.panda)
+        self.person = Person('firstname', 15, ['aaa', 'bbb', self.panda], {}, self.panda)
         self.panda_xml_string = '<Panda><name>Georgi</name><age>1</age></Panda>'
         self.panda_json_string = """{
     "dict": {
@@ -18,19 +18,17 @@ class TestJsonXml(unittest.TestCase):
         self.person_xml_string = '<Person><first_name>Georgi</first_name><last_name>Georgiev</last_name><age>25</age></Person>'
         self.person_json_string = """{
     "dict": {
-        "first_name": "joro",
-        "last_name": "georgiev",
+        "name": "joro",
         "age": 20,
         "friends": [
-            "sasho",
             "stoyan",
             "svetlio",
             {
                 "dict": {
-                    "first_name": "goshko",
-                    "last_name": "goshev",
+                    "name": "goshko",
                     "age": 5,
                     "friends": [],
+                    "children": {},
                     "panda": {
                         "dict": {
                             "name": "ivcho",
@@ -42,6 +40,35 @@ class TestJsonXml(unittest.TestCase):
                 "type": "Person"
             }
         ],
+        "children": {
+            "boys": {
+                "count": 2,
+                "children": [
+                    "plamen",
+                    "pavel",
+                    {
+                        "dict": {
+                            "name": "goshko",
+                            "age": 5,
+                            "friends": [],
+                            "children": {},
+                            "panda": {
+                                "dict": {
+                                    "name": "ivcho",
+                                    "age": 2
+                                },
+                                "type": "Panda"
+                            }
+                        },
+                        "type": "Person"
+                    }
+                ]
+            },
+            "girls": {
+                "count": 0,
+                "children": []
+            }
+        },
         "panda": {
             "dict": {
                 "name": "ivcho",
@@ -51,7 +78,8 @@ class TestJsonXml(unittest.TestCase):
         }
     },
     "type": "Person"
-}"""
+}
+        """
 
     def test_from_json(self):
         self.assertIsInstance(Panda.from_json(self.panda_json_string), Panda)
@@ -74,5 +102,3 @@ class TestJsonXml(unittest.TestCase):
     def test_output(self):
         self.assertRaises(ValueError, Panda.from_xml, self.person_xml_string)
         self.assertRaises(ValueError, Person.from_json, self.panda_json_string)
-
-
